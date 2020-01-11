1333c5805d0639f50744846a22e23faa427d9590
Lukas Pfannschmidt <lukas@lpfann.me>
2020-01-11 17:02:19 +0100

    remove saving of imps and new structure of checks

--- squamish/algorithm.py
+++ squamish/algorithm.py
@@ -22,7 +22,19 @@ def get_combined_set_without_f(f, MR, W):
     C = np.sort(C).astype(int)
     return C
 
-
+def is_significant_score_deviation(score_without_f,null_distribution):
+        # check score if f is removed
+        print(f"removal_score:{score_without_f:.3}-> ", end="")
+        # Test if value lies in acceptance range of null distribution
+        # i.e. no signif. change compared to perm. feature
+        # __We only check lower dist bound for worsening score when f is removed -> Strong relevant
+        if score_without_f < null_distribution[0]:
+            print(f"S")
+            return True
+        else:
+            print(f"W")
+            return False
+        
 def sort_features(X, y, MR, AR):
 
     S = []
@@ -56,55 +68,39 @@ def sort_features(X, y, MR, AR):
     imps = np.zeros((len(MR), X.shape[1]))
 
     # TODO: Iteration over M u RR
-    for i, f in enumerate(MR):
+    for f in MR:
         print("-------------------")
-        print(f"Feature i:{i} f:{f}")
+        print(f"Feature f:{f}")
         # Remove feature f from MR u W
         fset_without_f = get_combined_set_without_f(f, MR, W)
 
-        # check score if f is removed
-        score_without_f = model.redscore(X, y, fset_without_f)
-        print(f"removal_score:{score_without_f:.3}-> ", end="")
-        # Test if value lies in acceptance range of null distribution
-        # i.e. no signif. change compared to perm. feature
-        # __We only check lower dist bound for worsening score when f is removed -> Strong relevant
-        if score_without_f < score_bounds[0]:
-            print(f"S")
+        # Determine Relevance class by checking score without feature f
+        score_without_f = model.redscore(X,y, fset_without_f)
+        significant = is_significant_score_deviation(score_without_f,score_bounds)
+        if significant:
             S.append(f)
         else:
-            print(f"W")
             W.append(f)
 
         #
-        #
         # Record Importances with this subset of features
-        imps_without_f = model.importances()
-        imps[i, fset_without_f] = imps_without_f
-        # Replace current importance for feature f with median as neutral element
-        # imps[i,i] = np.median(imps_without_f)
-        imps[
-            i, i
-        ] = (
-            np.nan
-        )  # Replace current importance for feature f with median as neutral element
-        # TODO: correct checks?
-        related[f] = check_related(imps_without_f, imp_bounds_list)
-        print(related[f])
+        if not significant:
+            ix_and_imps = zip(fset_without_f,model.importances())
+            related[f] = check_related(ix_and_imps,imp_bounds_list)
+
         
     print("Related:",related)
     return S, W, imps, normal_imps, imp_bounds_list
 
 
-def check_related(importances_i, imp_bounds):
+def check_related(importances_other, importances_null_bounds):
     cands = []
-    for j in range(len(importances_i)):
-
-        lo,hi = imp_bounds[j]
-        imp = importances_i[j]
+    for f_ix,imp in importances_other:
+        lo,hi = importances_null_bounds[f_ix]
         if lo <= imp <= hi:
             # No change in relation to null dist
             continue
         else:
-            print(j, lo, imp, hi)
-            cands.append(j)
+            print(f_ix, lo, imp, hi)
+            cands.append(f_ix)
     return cands
